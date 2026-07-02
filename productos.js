f1c6fec2d3c048aed4ed771ca5c7a622e21ce649
Brunich17 <lgonzaless22_1@unc.edu.pe>
2026-07-02 00:07:35 -0500

    commit_productos
    commit_productos

--- productos.js
+++ productos.js
@@ -1,14 +1,14 @@
 const productos = [
-  { id: 1, nombre: 'BILOX CAP', precio: 1.50, categoria: 'Cápsulas', imagen: 'img/productos/bilox.jpg' },
-  { id: 2, nombre: 'NEURODIXAFLEX', precio: 4.00, categoria: 'Cápsulas', imagen: 'img/productos/neurodixaflex.jpg' },
-  { id: 3, nombre: 'WARMI CP', precio: 3.50, categoria: 'Cápsulas', imagen: 'img/productos/warmi.jpg' },
-  { id: 4, nombre: 'VITACOSE PLUS', precio: 28.50, categoria: 'Vitaminas', imagen: 'img/productos/vitacose.jpg' },
-  { id: 5, nombre: 'B-VATFORT', precio: 23.00, categoria: 'Vitaminas', imagen: 'img/productos/bvatfort.jpg' },
-  { id: 6, nombre: 'WELTONIC', precio: 50.00, categoria: 'Jarabes', imagen: 'img/productos/weltonic.jpg' },
-  { id: 7, nombre: 'NEUROPRESS', precio: 4.50, categoria: 'Tabletas', imagen: 'img/productos/neuropress.jpg' },
-  { id: 8, nombre: 'ELITON CIP', precio: 46.00, categoria: 'Jarabes', imagen: 'img/productos/eliton.jpg' },
-  { id: 9, nombre: 'PARACETAMOL 500 MG', precio: 2.00, categoria: 'Analgésicos', imagen: 'img/productos/paracetamol.jpg' },
-  { id: 10, nombre: 'IBUPROFENO 400 MG', precio: 2.50, categoria: 'Analgésicos', imagen: 'img/productos/ibuprofeno.jpg' },
-  { id: 11, nombre: 'VITAMINA C', precio: 12.00, categoria: 'Vitaminas', imagen: 'img/productos/vitamina-c.jpg' },
-  { id: 12, nombre: 'ALCOHOL MEDICINAL', precio: 8.50, categoria: 'Cuidado', imagen: 'img/productos/alcohol.jpg' }
+  { id: 1, nombre: 'BILOX CAP', precio: 1.50, categoria: 'Cápsulas', imagen: 'bilox.jpg' },
+  { id: 2, nombre: 'NEURODIXAFLEX', precio: 4.00, categoria: 'Cápsulas', imagen: 'neurodixaflex.jpg' },
+  { id: 3, nombre: 'WARMI CP', precio: 3.50, categoria: 'Cápsulas', imagen: 'warmi.jpg' },
+  { id: 4, nombre: 'VITACOSE PLUS', precio: 28.50, categoria: 'Vitaminas', imagen: 'vitacose.jpg' },
+  { id: 5, nombre: 'B-VATFORT', precio: 23.00, categoria: 'Vitaminas', imagen: 'bvatfort.jpg' },
+  { id: 6, nombre: 'WELTONIC', precio: 50.00, categoria: 'Jarabes', imagen: 'weltonic.jpg' },
+  { id: 7, nombre: 'NEUROPRESS', precio: 4.50, categoria: 'Tabletas', imagen: 'neuropress.jpg' },
+  { id: 8, nombre: 'ELITON CIP', precio: 46.00, categoria: 'Jarabes', imagen: 'eliton.jpg' },
+  { id: 9, nombre: 'PARACETAMOL 500 MG', precio: 2.00, categoria: 'Analgésicos', imagen: 'paracetamol.jpg' },
+  { id: 10, nombre: 'IBUPROFENO 400 MG', precio: 2.50, categoria: 'Analgésicos', imagen: 'ibuprofeno.jpg' },
+  { id: 11, nombre: 'VITAMINA C', precio: 12.00, categoria: 'Vitaminas', imagen: 'vitamina-c.jpg' },
+  { id: 12, nombre: 'ALCOHOL MEDICINAL', precio: 8.50, categoria: 'Cuidado', imagen: 'alcohol.jpg' }
 ];
