39d447e0dd84666094d2a265a061e5891dd20a57
Brunich17 <lgonzaless22_1@unc.edu.pe>
2026-07-02 00:13:01 -0500

    commit7
    commit7

--- productos.js
+++ productos.js
@@ -1,4 +1,4 @@
-const productos = [
+window.productos = [
   { id: 1, nombre: 'BILOX CAP', precio: 1.50, categoria: 'Cápsulas', imagen: 'bilox.jpg' },
   { id: 2, nombre: 'NEURODIXAFLEX', precio: 4.00, categoria: 'Cápsulas', imagen: 'neurodixaflex.jpg' },
   { id: 3, nombre: 'WARMI CP', precio: 3.50, categoria: 'Cápsulas', imagen: 'warmi.jpg' },
@@ -6,9 +6,5 @@ const productos = [
   { id: 5, nombre: 'B-VATFORT', precio: 23.00, categoria: 'Vitaminas', imagen: 'bvatfort.jpg' },
   { id: 6, nombre: 'WELTONIC', precio: 50.00, categoria: 'Jarabes', imagen: 'weltonic.jpg' },
   { id: 7, nombre: 'NEUROPRESS', precio: 4.50, categoria: 'Tabletas', imagen: 'neuropress.jpg' },
-  { id: 8, nombre: 'ELITON CIP', precio: 46.00, categoria: 'Jarabes', imagen: 'eliton.jpg' },
-  { id: 9, nombre: 'PARACETAMOL 500 MG', precio: 2.00, categoria: 'Analgésicos', imagen: 'paracetamol.jpg' },
-  { id: 10, nombre: 'IBUPROFENO 400 MG', precio: 2.50, categoria: 'Analgésicos', imagen: 'ibuprofeno.jpg' },
-  { id: 11, nombre: 'VITAMINA C', precio: 12.00, categoria: 'Vitaminas', imagen: 'vitamina-c.jpg' },
-  { id: 12, nombre: 'ALCOHOL MEDICINAL', precio: 8.50, categoria: 'Cuidado', imagen: 'alcohol.jpg' }
+  { id: 8, nombre: 'ELITON CIP', precio: 46.00, categoria: 'Jarabes', imagen: 'eliton.jpg' }
 ];
